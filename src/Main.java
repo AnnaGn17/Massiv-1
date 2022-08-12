7bd425acba17f34fb07b6a721a393884507b91df
homework-2 <an_1704_na@inbox.ru>
2022-08-12 13:15:10 +0300

    11

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-import java.sql.SQLOutput;
 
-public class Main {
+
+public class Main  {
     public static void main(String[] args) {
         //   task 1
         int weight [] = new int[3];
@@ -10,32 +10,35 @@ public class Main {
         double drob [] = {1.57, 7.654, 9.986};
         char arch [] = {65, 66, 67};
 //   task 2
-        for (int i = 0; i <= 2; i ++){
-            if (i < 2) System.out.print(weight[i] + ",");
-            else System.out.println(weight[i]);}
+        for (int i = 0; i < weight.length ; i ++) {
+            if (i == weight.length) System.out.println(weight[i]);
+            else System.out.print(weight[i] + ",");}
 
-        for (int k = 0; k <= 2; k++) {
-            if (k < 2) System.out.print(drob[k] + ",");
-            else System.out.println(drob[k]);}
+        for (int k = 0; k < drob.length; k++) {
+            if (k == drob.length) System.out.println(drob[k]);
+            else System.out.print(drob[k] + ",");}
 
-        for (int l = 0; l <= 2; l++) {
-            if (l < 2) System.out.print(arch[l] + ",");
-            else System.out.println(arch[l]);}
+        for (int l = 0; l < arch.length; l++) {
+            if (l == arch.length) System.out.println(arch[l]);
+            else System.out.print(arch[l] + ",");}
 //   task 3
-        for (int q = 2; q >= 0; q--) {
-            if (q > 0) System.out.print(weight[q] + ",");
-            else System.out.println(weight[q]);}
+        int h = weight.length - 1;
+        for (int q = h; q >= 0; q--) {
+            if (q == 0) System.out.println(weight[q]);
+            else System.out.print(weight[q] + ",");}
 
-        for (int w = 2; w >= 0; w--) {
-            if (w > 0) System.out.print(drob[w] + ",");
-            else System.out.println(drob[w]);}
+        int j = drob.length - 1;
+        for (int w = j; w >= 0; w--) {
+            if (w == 0) System.out.println(drob[w]);
+            else System.out.print(drob[w] + ",");}
 
-        for (int e = 2; e >= 0; e--) {
-            if (e > 0) System.out.print(arch[e] + ",");
-            else System.out.println(arch[e]);}
+        int k = arch.length - 1;
+        for (int e = k; e >= 0; e--) {
+            if (e == 0) System.out.println(arch[e]);
+            else System.out.print(arch[e] + ",");}
 
 //   task 4
-        for (int n = 0; n <= 2; n++) {
+        for (int n = 0; n < weight.length; n++) {
             if (weight[n] % 2 == 1) {
                 weight[n]++;
                 if (n < 2)
